af1bad4909915f21212335a4c5ae1d8310079ab0
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-05-03 11:08:28 +0200

    Finished project, all should work fine

--- Matrixes/matrix.c
+++ Matrixes/matrix.c
@@ -49,97 +49,147 @@ void print_mat(int rows, int cols, int* t)
 // 2
 /////////////////////////////////////////////////////////////
 
-int read_int_lines_cont(int* ptr_array[], int* row_len) 
+int read_int_lines_cont(int *ptr_array[]) 
+{
+	// Init:
+	int* current = ptr_array[0];
+	int row = 0;
+	
+	for(int i=0; i<TAB_SIZE; i++)
+	{
+		*current = 0;
+		current++;
+	}
+	current = ptr_array[0];
+
+	// Input variables:
+	char c;
+	int sign = 1; // negative/positive numbers
+
+	// Main loop:
+	while((c = getchar()) != EOF)
+	{
+		//printf("Wczytuję: %c\n", c);
+		if(c == '\n')
+		{
+			// Go to new number, as if c was ' ':
+				// End processing prevoius number:
+				*current *= sign;
+				sign = 1;
+
+				// Get to space for new number:
+				current++;
+			
+			// Point at the new number to indicate a new row:
+			row++;
+			ptr_array[row] = current;
+
+			continue;
+		}
+
+		if(c == ' ') // Go to new number
+		{
+			// End processing prevoius number:
+			*current *= sign;
+			sign = 1;
+
+			// Get to space for new number:
+			current++;;
+		}
+		else 
+		{
+			if(c == '-')
+			{
+				sign = -1;
+				continue;
+			}
+			// Add character to number:
+			*current *= 10;
+			*current += (c - '0');
+		}
+	}
+
+	//for(int i=0; i<5; i++)
+	//	printf("\n%d\n", *ptr_array[i]);
+	return row;
+}
+
+void write_int_line_cont(int *ptr_array[], int n) 
+{
+	//printf("zaczynam wypisywać linię\n");
+	int* i = ptr_array[n];
+	while(i != ptr_array[n+1])
+	{
+		printf("%d ", *i);
+		i++;
+	}
+	//printf("\nkończę wypisywać linię\n");
+}
+
+// 3
+///////////////////////////////////////////////////////////
+
+
+int read_char_lines(char *array[]) 
 {
 	// Init:
 	int line = 0;
 	int len = 0;
 	char c;
-	int sign = 1;
 
 	// Prepere first line:
-	ptr_array[0] = malloc(TAB_SIZE * sizeof(int));
+	array[0] = malloc(TAB_SIZE * sizeof(char));
 
 	// Main loop:
 	while((c = getchar()) != EOF)
 	{
 		if(c == '\n') // End of line
 		{
-			ptr_array[line] = (int*)realloc(ptr_array[line], (len+1)*sizeof(int));
-			row_len[line] = len+1;
+			// Append '\0':
+			array[line][len] = '\0';
+			len++;
 
-			sign = 1;
+			array[line] = realloc(array[line], (len+1)*sizeof(char));
+			
+			// Write down the progress
 			line ++;
-			printf("Długość: %d\n", len);
-			len=0;
-			row_len[line] = 0;
 
 			// Prepere next row:
+			len=0;
 			if(line < TAB_SIZE)
 			{
-				ptr_array[line] = malloc(TAB_SIZE * sizeof(int));
-				ptr_array[line][0] = 0; 
+				array[line] = malloc(TAB_SIZE * sizeof(char));
 			}
 
-			continue;
+			continue; // Don't append the '\n'
 		}
 
-		if(c == ' ') // Go to next array space
-		{
-			ptr_array[line][len] *= sign;
-			sign = 1;
-
-			len++;
-			ptr_array[line][len] = 0;
-		}
-
-		if(c == '-')
-		{
-			sign = -1;
-			continue;
-		}
-
-		if(c != ' ') // Append number to array:
-		{
-			ptr_array[line][len] *= 10; 
-			ptr_array[line][len] += c - 48;
-		}
+		// Append number to array: 
+		array[line][len] = c;
+		len++;
 	}
 	// We know that every line is ended with \n, so we have to clean up last line, which was prepered (malloc()) but never used
-	free(ptr_array[line]);
+	free(array[line]);
 
 	return line;
 }
 
-void write_int_line_cont(int *ptr_array[], int* n, int* row_len) 
+void write_char_line(char *array[], int n) 
 {
-	for(int i=0; i<	row_len[*n]; i++)
-		printf("%d ", ptr_array[*n][i]);
-	printf("\n");
-}
-
-void cleanup(int *ptr_array[], int n)
-{
-	for(int i=0; i<n; i++)
+	int i=0;
+	while(array[n][i] != '\0')
 	{
-		//printf("%d\n", i);
-		free(ptr_array[i]);
+		printf("%c", array[n][i]);
+		i++;
 	}
 }
 
-// 3
-///////////////////////////////////////////////////////////
-
-/*
-int read_char_lines(char *array[]) {
+void delete_lines(char *array[], int n) 
+{
+	for(int i=0; i<n; i++)
+		free(array[i]);
 }
 
-void write_char_line(char *array[], int n) {
-}
-
-void delete_lines(char *array[]) {
-}
-*/
 
 // auxiliary
 ////////////////////////////////////////////////////////////
@@ -151,29 +201,17 @@ int read_int(void) {
 }
 
 int main(void) 
-{
-	// DEBUG:
-	//const int to_do = read_int();
-	//int A[TAB_SIZE];
-	/*
-	read_mat(2, 3, A);
-	read_mat(3, 2, B);
-	prod_mat(2, 3, 3, A, B, AB);
-	*/
-	//printf("%d\n", get(3, 1, 0, A));
-
-	// PROPER CODE:
-	
+{	
 	const int to_do = read_int();
 
 	int A[TAB_SIZE], B[TAB_SIZE], AB[TAB_SIZE];
 	int n, rowsA, colsA, rowsB, colsB;
 	char* char_lines_array[TAB_SIZE] = { NULL };
-	int continuous_array[TAB_SIZE];
 	int* ptr_array[TAB_SIZE];
-	int row_len[TAB_SIZE];
+	int continuous_array[TAB_SIZE];
 
 	switch (to_do) {
+
 		case 1:
 			scanf("%d %d", &rowsA, &colsA);
 			read_mat(rowsA, colsA, A);
@@ -182,21 +220,22 @@ int main(void)
 			prod_mat(rowsA, colsA, colsB, A, B, AB);
 			print_mat(rowsA, colsB, AB);
 			break;
+
 		case 2:
 			n = read_int() - 1; // we count from 1 :)
-			//ptr_array[0] = continuous_array; Why is it even here? It was in original template so I won't delete it
-			int arr_size = read_int_lines_cont(ptr_array, row_len);
-			write_int_line_cont(ptr_array, &n, row_len);
-			cleanup(ptr_array, arr_size);
+			//printf("n = %d\n", n);
+			ptr_array[0] = continuous_array;
+			read_int_lines_cont(ptr_array);
+			write_int_line_cont(ptr_array, n);
 			break;
-			/*
+
 		case 3:
 			n = read_int() - 1;
-			read_char_lines(char_lines_array);
+			int rows = read_char_lines(char_lines_array);
 			write_char_line(char_lines_array, n);
-			delete_lines(char_lines_array);
+			delete_lines(char_lines_array, rows);
 			break;
-			*/
+
 		default:
 			printf("NOTHING TO DO FOR %d\n", to_do);
 			break;
