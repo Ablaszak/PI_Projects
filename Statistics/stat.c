e1bf4c3ed69c71659f399c270a35e74fb783b6dd
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-05-10 20:46:14 +0200

    wip

--- Statistics/stat.c
+++ Statistics/stat.c
@@ -135,7 +135,67 @@ double max (const int number, const double* values) {
 
 int read_from_line(char* c_buf, double *values, char** texts, int* text_counter) 
 {
+	// Init:
+	*text_counter = 0;
+	int devider = 1;
+	char c;
+	int instring = 0, got_int = 0, past_coma = 0; // bool
+	int val_it = 0; // iterator
 	
+	for(int i=0; i<MAX_NUMBER; i++)
+		values[i] = 0;
+	for(int i=0; i<MAX_TEXTS; i++)
+		texts[i] = "";
+
+	// Main loop:
+	while((c = getchar()) != EOF)
+	{
+		if(c == ' ')
+		{
+			if(instring == 0) // edited just int, not a string
+			{
+				values[val_it] *= devider;
+				devider = 1;
+				val_it ++;
+			}
+			else 			  // edited string (int edition was ended elsewhere)
+				*text_counter ++;
+
+			instring = 0;
+			got_int = 0;
+			continue;
+		}
+
+		if(isalnum(c) && instring == 0)
+		{
+			values[val_it] *= 10;
+			valuse[val_it] += int(c - '0');
+			
+			if(past_coma == 1)
+				devider /= 10;
+
+			got_int = 1;
+			continue;
+		}
+
+		if(c == '.' && instring == 0) // going past coma in a number
+		{
+			devider = 1;
+			continue;
+		}
+
+		// Now we enter the string teritory
+		isntring = 1;
+
+		if(got_int == 1) // We've just ended an int
+		{
+			values[val_it] *= devider;
+			devider = 1;
+			val_it ++;
+		}
+
+		strncat(texts[*text_counter], &c, 1);
+	}
 }
 
 int read_int(void) {
